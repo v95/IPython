1202275d6bf788667bb0761b0edea842ee1d004a
badshar <saurabh-2007@hotmail.com>
2015-05-06 15:42:45 -0400

    Initialization

---
+++ IPython/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IPython.Global" Language="C#" %>
---
+++ IPython/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IPython.Startup))]
+namespace IPython
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
